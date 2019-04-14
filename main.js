09f3cfe6cbee8f6842323423e1b7f913bdcc37a9
wjdwoduq <jjy37777@naver.com>
2019-04-14 20:34:58 +0900

    first commit

--- main.js
+++ main.js
@@ -1,35 +1,15 @@
+const http = require('http');
 
-// const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-var express = require('express');
-var app = express();
-
-app.set("view engine","ejs");
-app.use(express.static(__dirname + '/public'));
-
-app.get("/hello", function(req,res){
-  res.render("hello", {name:req.query.nameQuery});
+  res.end('Hello World\n');
 });
 
-app.get("/hello/:nameParam", function(req,res){
-  res.render("hello", {name:req.params.nameParam});
-});
-
-var port = process.env.PORT || 3000; //*
-app.listen(port, function(){
-  console.log('Server On!');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
