04302b0a5940446eb8d2ffce9243872607a00b26
wjdwoduq <jjy37777@naver.com>
2019-04-14 21:14:04 +0900

    Sasdf

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   
-  res.end('Hello World\n');
+  res.end('WOW My First NodeJs WOW');
 });
 
 server.listen(port, hostname, () => {
