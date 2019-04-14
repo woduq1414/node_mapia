9bfa7ab572c77faa20a5f0790c15b04a3f916237
wjdwoduq <jjy37777@naver.com>
2019-04-14 21:02:56 +0900

    sdaf

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+// const hostname = '127.0.0.1';
+// const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
   
-  res.end('Hello World\n');
-});
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// //sadfsdfs
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
-//sadfsdfs
