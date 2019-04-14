acb690d3a839a12e72835be7eee6625d83ac7422
wjdwoduq <jjy37777@naver.com>
2019-04-14 21:08:24 +0900

    dd

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Serveeeeeeeeeeeeeeer running at http://${hostname}:${port}/`);
 });
 //sadfsdfs
